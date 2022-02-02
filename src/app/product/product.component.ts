4db87902d62f86651aae2cd2c80bf58c49174638
AhmedTharwat17 <92866219+AhmedTharwat17@users.noreply.github.com>
2022-02-02 20:07:11 +0200

    use Event Binding ,Template reference variable , Two way Binding , Structural directives , Pipes

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -6,6 +6,7 @@ import { IPerson , ICategory , DiscountOffers } from '../Shared Classes and type
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.scss']
 })
+
 export class ProductComponent implements OnInit {
   Discount:DiscountOffers;
   Storename:string;
@@ -15,39 +16,52 @@ export class ProductComponent implements OnInit {
   ClientName:string;
   IsPurshased:boolean;
   constructor() {
-    this.Discount = DiscountOffers.up;
+    this.Discount = DiscountOffers.down;
     this.Storename = "EGD Store";
-    this.StoreLogo = "../../assets/images/product.jpg";
+    this.StoreLogo = "../../assets/images/background.jpg";
     this.ProductList = [
       {
         ID:1,
-        Name:"Shirt",
+        Name:"Sweatshirt",
         Quantity:80,
-        Price:500,
-        Img:"../../assets/logo.png"
+        Price:400,
+        Img:"../../assets/images/product1.jpg"
       },
       {
         ID:2,
-        Name:"cap",
+        Name:"trouser",
         Quantity:50,
-        Price:100,
-        Img:"../../assets/logo.png"
+        Price:300,
+        Img:"../../assets/images/product2.jpg"
+      },
+      {
+        ID:3,
+        Name:"Shose",
+        Quantity:70,
+        Price:250,
+        Img:"../../assets/images/product3.jpg"
       }
     ];
     this.CategoryList = [
       {
         ID:1,
-        Name:"A"
+        Name:"Electronics"
       },
       {
         ID:2,
-        Name:"B"
+        Name:"Groceries"
+      },
+      {
+        ID:3,
+        Name:"Crafts&Sewing"
       }
     ]
-    this.ClientName = "Ahmed",
-    this.IsPurshased = true
+    this.ClientName = "Ahmed Tharwat",
+    this.IsPurshased = false
+  }
+  toggleBuy(){
+    this.IsPurshased = !this.IsPurshased;
   }
   ngOnInit(): void {
   }
-
 }
